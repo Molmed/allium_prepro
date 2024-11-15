22848fb9322ca98dbb2a3518a620fbe368a63252
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2024-11-15 10:47:00 +0100

    Bugfix in umap script

--- mpm_experiments/batch_umap.py
+++ mpm_experiments/batch_umap.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 from src.allium_prepro.batch_umap import BatchUmap
 
-DATASET_PREFIX = 'lilljebjorn'
+DATASET_PREFIX = 'heinaniemi'
 
 # Data
 data_dir = '/home/mariya/Data/allium'
@@ -11,8 +11,9 @@ pheno_file = f'{data_dir}/{DATASET_PREFIX}.pheno.allium.csv'
 counts_file = f'{data_dir}/{DATASET_PREFIX}.counts.allium.csv'
 batches_file = f'{data_dir}/{DATASET_PREFIX}.batches.allium.csv'
 
-bu = BatchUmap(prefix={DATASET_PREFIX},
+bu = BatchUmap(prefix=DATASET_PREFIX,
                counts_file=counts_file,
                batches_file=batches_file,
                phenotype_file=pheno_file,
                output_dir=data_dir)
+bu.run()
